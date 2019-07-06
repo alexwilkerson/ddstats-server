b41c391985b2b821bf374e3023f9d45bfd6a7058
Alex Wilkerson <alex@uncorrected.com>
2019-07-06 16:35:45 -0500

    trying things out

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/alexwilkerson/ddstats-api
+
+go 1.12
---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/alexwilkerson/ddstats-api/server"
+)
+
+var (
+	CertFile      = os.Getenv("CERT_FILE")
+	KeyFile       = os.Getenv("KEY_FILE")
+	ServerAddress = os.Getenv("SERVER_ADDRESS")
+)
+
+const (
+	message = "Hello, World."
+)
+
+func main() {
+	logger := log.New(os.Stdout, "[ddstats-api] ", log.LstdFlags)
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", HomeHandler)
+
+	err := server.New(mux, ServerAddress).ListenAndServeTLS(CertFile, KeyFile)
+	if err != nil {
+		log.Fatalf("server failed to start: %v", err)
+	}
+}
+
+func HomeHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(message))
+}
