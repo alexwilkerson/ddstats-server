fd0b53cf4a9e6d1b5dc733a6ddc6d86865e24f4e
Alex Wilkerson <alex@uncorrected.com>
2019-12-31 00:24:26 -0600

    added basic route

--- handlers.go
+++ handlers.go
@@ -2,8 +2,31 @@ package main
 
 import (
 	"net/http"
+	"strconv"
 )
 
 func (app *application) helloWorld(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello, Merle!"))
+}
+
+func (app *application) ddGetUserByRank(w http.ResponseWriter, r *http.Request) {
+	rank, ok := r.URL.Query()["rank"]
+	if !ok || len(rank) < 1 {
+		app.clientError(w, http.StatusBadRequest)
+		return
+	}
+
+	rankInt, err := strconv.Atoi(rank[0])
+	if err != nil {
+		app.clientError(w, http.StatusBadRequest)
+		return
+	}
+
+	if rankInt < 1 {
+		app.clientError(w, http.StatusBadRequest)
+		return
+	}
+
+	w.Write([]byte(strconv.Itoa(rankInt)))
+
 }
