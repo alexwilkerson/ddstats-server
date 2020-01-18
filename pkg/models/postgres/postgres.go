3afd8d46cbec94c82775a97cc64eb5f7bc4a06dc
Alex Wilkerson <alex@uncorrected.com>
2020-01-17 23:39:56 -0600

    added new, active player models

--- pkg/models/postgres/postgres.go
+++ pkg/models/postgres/postgres.go
@@ -7,33 +7,37 @@ import (
 )
 
 type Postgres struct {
-	DB                  *sqlx.DB
-	Games               *GameModel
-	States              *StateModel
-	Players             *PlayerModel
-	SubmittedGames      *SubmittedGameModel
-	MOTD                *MOTDModel
-	DiscordUsers        *DiscordUserModel
-	Releases            *ReleaseModel
-	News                *NewsModel
-	CollectorRuns       *CollectorRunModel
-	CollectorPlayers    *CollectorPlayerModel
-	CollectorHighScores *CollectorHighScoreModel
+	DB                     *sqlx.DB
+	Games                  *GameModel
+	States                 *StateModel
+	Players                *PlayerModel
+	SubmittedGames         *SubmittedGameModel
+	MOTD                   *MOTDModel
+	DiscordUsers           *DiscordUserModel
+	Releases               *ReleaseModel
+	News                   *NewsModel
+	CollectorRuns          *CollectorRunModel
+	CollectorPlayers       *CollectorPlayerModel
+	CollectorHighScores    *CollectorHighScoreModel
+	CollectorActivePlayers *CollectorActivePlayerModel
+	CollectorNewPlayers    *CollectorNewPlayerModel
 }
 
 func NewPostgres(client *http.Client, db *sqlx.DB) *Postgres {
 	return &Postgres{
-		DB:                  db,
-		Games:               &GameModel{DB: db},
-		States:              &StateModel{DB: db},
-		Players:             &PlayerModel{DB: db},
-		SubmittedGames:      &SubmittedGameModel{DB: db, Client: client},
-		MOTD:                &MOTDModel{DB: db},
-		DiscordUsers:        &DiscordUserModel{DB: db},
-		Releases:            &ReleaseModel{DB: db},
-		News:                &NewsModel{DB: db},
-		CollectorRuns:       &CollectorRunModel{DB: db},
-		CollectorPlayers:    &CollectorPlayerModel{DB: db},
-		CollectorHighScores: &CollectorHighScoreModel{DB: db},
+		DB:                     db,
+		Games:                  &GameModel{DB: db},
+		States:                 &StateModel{DB: db},
+		Players:                &PlayerModel{DB: db},
+		SubmittedGames:         &SubmittedGameModel{DB: db, Client: client},
+		MOTD:                   &MOTDModel{DB: db},
+		DiscordUsers:           &DiscordUserModel{DB: db},
+		Releases:               &ReleaseModel{DB: db},
+		News:                   &NewsModel{DB: db},
+		CollectorRuns:          &CollectorRunModel{DB: db},
+		CollectorPlayers:       &CollectorPlayerModel{DB: db},
+		CollectorHighScores:    &CollectorHighScoreModel{DB: db},
+		CollectorActivePlayers: &CollectorActivePlayerModel{DB: db},
+		CollectorNewPlayers:    &CollectorNewPlayerModel{DB: db},
 	}
 }
