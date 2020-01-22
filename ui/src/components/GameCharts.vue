52a7de6c8c2dba81ae8436792fda23ce9f4dc44c
Alex Wilkerson <alex@uncorrected.com>
2020-01-22 00:05:27 -0600

    got some things working

---
+++ ui/src/components/GameCharts.vue
@@ -0,0 +1,22 @@
+<template>
+  <p>{{ info }}</p>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  data() {
+    return {
+      info: null
+    };
+  },
+  mounted() {
+    axios
+      .get(process.env.VUE_APP_API_URL + "/api/v2/game/all?id=2")
+      .then(response => (this.info = response))
+      .catch(error => console.log(error));
+  }
+};
+</script>
+
+<style></style>
