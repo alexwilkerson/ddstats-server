9d43d09183af67a754c6d244db839d259396249e
Alex Wilkerson <alexwilkerson@users.noreply.github.com>
2026-08-08 16:33:09 -0600

    Truncate long player names in Top/Recent Games with hover tooltip (#45)
    Long names were pushing the game time column off-screen. Cap the name
    column to available width with an ellipsis, size the time column to
    its own content instead of letting it balloon, and show the full name
    via the title attribute on hover.

--- ui/src/components/Home/RecentGames.vue
+++ ui/src/components/Home/RecentGames.vue
@@ -6,15 +6,15 @@
     <v-simple-table v-if="!loading">
       <tbody>
         <tr v-for="game in data.games" :key="game.id" @click="selectGame(game)" class="pointer">
-          <td class="dd-card-list-item-bold">
+          <td class="dd-card-list-item-bold truncate-name">
             <v-icon
               v-if="$root.checkPlayerLive(game.player_id)"
               class="icon online-green"
               small
             >mdi-access-point</v-icon>
-            {{ game.player_name }}
+            <span class="truncate-name-text" :title="game.player_name">{{ game.player_name }}</span>
           </td>
-          <td class="dd-card-list-item text-right">
+          <td class="dd-card-list-item text-right shrink-column">
             {{ game.game_time.toFixed(4) }}s
             <br />
             <span class="since-text">{{ moment(game.time_stamp).fromNow() }}...</span>
@@ -75,5 +75,23 @@ export default {
   font-family: "alte_haas_grotesk", "Helvetica Neue", Helvetica, Arial;
   font-style: oblique;
   font-size: 12px !important;
+}
+
+.truncate-name {
+  max-width: 0;
+}
+
+.shrink-column {
+  width: 1%;
+  white-space: nowrap;
+}
+
+.truncate-name-text {
+  display: inline-block;
+  max-width: 100%;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  vertical-align: middle;
 }
 </style>
